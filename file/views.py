a818f187e02837b753b81b47833f5ff113654bf1
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-03-28 00:26:24 +0300

    added views

--- file/views.py
+++ file/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from file.forms import UploadedFileForm
+from file.models import UploadedFile, ProcessedWord
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadedFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_file = form.save()
+            # Логика по подсчету tf-idf слов в файле
+    else:
+        form = UploadedFileForm()
+    return render(
+        request, 'file/upload_file.html', {'form': form}
+    )
+
+
+def file_list(request):
+    files = UploadedFile.objects.all()
+    return render(
+        request, 'file/file_list.html', {'files': files}
+    )
+
+
+# def processed_file_detail(request, file_id):
+#     processed_words = ProcessedWord.objects.filter(file=file_id).order_by('-idf')[:50]
+#     return render(
+#         request, 'file/file_detail.html', {'processed_words': processed_words}
+#     )
