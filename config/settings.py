9c9e67e874cbaf092e2d5f7f35de47bb25918d93
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-03-30 18:44:40 +0300

    added static

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'data/')
 MEDIA_URL = '/media/'
 
