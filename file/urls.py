dd6e43a5749a838192edca43339052b4e098cde5
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-03-30 16:48:26 +0300

    added urls

--- file/urls.py
+++ file/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from file.apps import FileConfig
-from file.views import upload_file, file_list, processed_file_detail
+from file.views import FileUploadView, UploadedFileList, UploadedFileDetailView
 
 app_name = FileConfig.name
 
 urlpatterns = [
-    path('upload/', upload_file, name='upload_file'),
-    path('files/', file_list, name='file_list'),
-    path('files/<int:file_id>/', processed_file_detail, name='file_detail'),
+    path('upload/', FileUploadView.as_view(), name='upload_file'),
+    path('files/', UploadedFileList.as_view(), name='file_list'),
+    path('files/<int:pk>/', UploadedFileDetailView.as_view(), name='file_detail'),
 ]
