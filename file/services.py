f0bb8a9a20529d6d3cce2cf2000f7bb852b50df2
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-03-30 16:47:57 +0300

    created func for text cleaning

---
+++ file/services.py
@@ -0,0 +1,28 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+stop_words = stopwords.words('russian')
+
+REGULAR = r'[^\w\s]'
+REGULAR_URL = r'(http\S+)|(www\S+)|([\w\d]+www\S+)|([\w\d]+http\S+)'
+
+
+def clean_text(text):
+    """
+    Функция очищает текст от специальных символов, ссылок, чисел и пробелов
+    """
+    text = text.lower()
+
+    text = re.sub(REGULAR, '', text)
+    text = re.sub(REGULAR_URL, r'', text)
+    text = re.sub(r'(\d+\s\d+)|(\d+)', '', text)
+    text = re.sub(r'\s+', ' ', text)
+
+    tokenized_text = word_tokenize(text, language='russian')
+
+    return [word for word in tokenized_text if word not in stop_words]
