5eb2e39abc45a4aed77cb362cee998bf0a3cfd1e
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2024-03-30 16:47:34 +0300

    models changes

--- file/models.py
+++ file/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+NULLABLE = {'null': True, 'blank': True}
 
 
 class UploadedFile(models.Model):
-    file = models.FileField(upload_to='data/', verbose_name='Файл')
+    file = models.FileField(upload_to='', verbose_name='Файл')
     uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата загрузки')
+    cleaned_text = ArrayField(models.CharField(max_length=100), verbose_name='Очищенный список слов', **NULLABLE)
 
     class Meta:
         verbose_name = 'Uploaded File'
@@ -11,16 +16,17 @@ class UploadedFile(models.Model):
         db_table = 'file'
 
     def __str__(self):
-        return f'{self.file}'
+        return str(self.file).split('/')[-1]
 
     def __repr__(self):
         return f'File({self.file}, {self.uploaded_at}'
 
 
 class ProcessedWord(models.Model):
+    file = models.ForeignKey(UploadedFile, on_delete=models.CASCADE, verbose_name='Файл')
     word = models.CharField(max_length=100, verbose_name='Слово')
-    tf = models.IntegerField(verbose_name='Частота слова')
-    idf = models.FloatField(verbose_name='Обратная частота документа')
+    tf = models.FloatField(verbose_name='Частота слова', **NULLABLE)
+    idf = models.FloatField(verbose_name='Обратная частота документа', **NULLABLE)
 
     class Meta:
         verbose_name = 'Processed Word'
